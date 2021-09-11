2d2b6b7c3e3b63593cb9353767b6ca00da8f6606
ShivanshVerma-coder <Shivansh29.sv@gmail.com>
2021-09-11 05:34:39 +0530

    routes setup

---
+++ config/db.js
@@ -0,0 +1,19 @@
+let mongoose = require("mongoose")
+let config = require("config")
+let db = config.get("mongoURI")
+
+let connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    console.log("MongoDB connected")
+  } catch (err) {
+    console.error(err.message)
+    //exit process with failure
+    process.exit(1)
+  }
+}
+
+module.exports = connectDB
---
+++ server.js
@@ -0,0 +1,18 @@
+let express = require("express")
+let connectDB = require("./config/db")
+
+let app = express()
+
+//connect Database
+connectDB()
+
+app.get("/", (req, res) => res.send("API Running"))
+
+app.use("/api/users", require("./routes/api/users"))
+app.use("/api/auth", require("./routes/api/auth"))
+app.use("/api/profile", require("./routes/api/profile"))
+app.use("/api/posts", require("./routes/api/posts"))
+
+let PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
